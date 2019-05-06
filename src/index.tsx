93580a738dd96c71078957cab493577212e33b5b
Jack Works <zjwpeter@gmail.com>
2019-05-06 12:32:16 +0800

    Add some examples

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 /// <reference path="./CustomElement.d.ts" />
 import './acrylic-reveal';
 import Button from '@material-ui/core/Button/Button';
+import Toolbar from '@material-ui/core/Toolbar/Toolbar';
+import { AppBar, Typography } from '@material-ui/core';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -23,6 +25,34 @@ ReactDOM.render(
     <h1>👇Custom Element</h1>
     {hello}
     {hello}
+    <acrylic-reveal>
+      <div style={{ width: 400, height: 200, background: 'cyan' }} />
+    </acrylic-reveal>
+    <acrylic-reveal>
+      <Button variant="contained" disableFocusRipple disableRipple disableTouchRipple>
+        Default
+      </Button>
+    </acrylic-reveal>
+    <acrylic-reveal>
+      <Button variant="contained" disableFocusRipple disableRipple disableTouchRipple color="primary">
+        Primary
+      </Button>
+    </acrylic-reveal>
+    <acrylic-reveal>
+      <Button variant="contained" disableFocusRipple disableRipple disableTouchRipple color="secondary">
+        Secondary
+      </Button>
+    </acrylic-reveal>
+    <acrylic-reveal>
+      <Button variant="contained" disableFocusRipple disableRipple disableTouchRipple color="secondary" disabled>
+        Disabled
+      </Button>
+    </acrylic-reveal>
+    <acrylic-reveal>
+      <Button variant="contained" disableFocusRipple disableRipple disableTouchRipple href="#contained-buttons">
+        Link
+      </Button>
+    </acrylic-reveal>
     <h1>👇React Component</h1>
   </acrylic-reveal-bound>,
   div
